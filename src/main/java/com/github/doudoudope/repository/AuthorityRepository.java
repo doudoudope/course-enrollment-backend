4bcb9249c6ca77c5c3adc0a57cf45b6754d08c9b
doudou <sarah.zy605@gmail.com>
2022-07-20 20:19:53 -0700

    Initial version of course_enroll_backend generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/github/doudoudope/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.github.doudoudope.repository;
+
+import com.github.doudoudope.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/github/doudoudope/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.github.doudoudope.service.dto;
